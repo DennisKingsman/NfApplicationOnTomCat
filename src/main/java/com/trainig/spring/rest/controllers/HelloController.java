f63a15e99070a0fd5f9f3564d6e826d2175db3c6
Dennis <den.kuznecov.1999@mail.ru>
2020-11-27 03:05:01 +0400

    simple RESTfull service solution

--- src/main/java/com/trainig/spring/rest/controllers/HelloController.java
+++ src/main/java/com/trainig/spring/rest/controllers/HelloController.java
@@ -1,12 +1,12 @@
-package com.trainigCenter.spring.restControllers;
+package com.trainig.spring.rest.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping(value = "/")
     public String index(){
         return "Hello world!";
     }
